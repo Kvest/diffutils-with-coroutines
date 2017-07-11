d92d124cff6003e5e3890c09de3d32043d8d2b15
Roman Bielokon <roman.bielokon@dhl.com>
2017-07-11 15:34:38 +0200

    initial commit

---
+++ app/src/androidTest/java/com/kvest/diffutils_with_coroutines/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.kvest.diffutils_with_coroutines;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.kvest.diffutils_with_coroutines", appContext.getPackageName());
+    }
+}
